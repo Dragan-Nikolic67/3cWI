543ab472e7377c6d04b53ced55033d60fa594699
Dragan-Nikolic67 <nesamskolovao2@gmail.com>
2023-09-19 09:22:00 +0200

    If bedingungen 1 fertig

---
+++ src/at/dragan/units/Ifbedingung2.java
@@ -0,0 +1,26 @@
+package at.dragan.units;
+
+import java.util.Random;
+
+public class Ifbedingung2 {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int randomNumber = random.nextInt(100);
+        System.out.print(randomNumber);
+
+        System.out.print(" ");
+
+        int randomNumber2 = random.nextInt(100);
+        System.out.println(randomNumber2);
+    if (randomNumber<randomNumber2 && randomNumber<50){
+        System.out.println("Zahl 1 ist kleiner als Zahl 2 und Mini");
+    }
+    if (randomNumber<30 || randomNumber2<30){
+        System.out.println("Eine der beiden ist kleiner als 30");
+    }
+    if (randomNumber<50 && randomNumber2!=50){
+        System.out.println("Erste Zahl klein, zweite kein 50iger");
+    }
+    }
+}
