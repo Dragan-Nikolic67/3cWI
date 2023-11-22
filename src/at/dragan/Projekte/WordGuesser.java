eafbb8d0dcb2537aab4352656a10225abe2c3ae7
Dragan-Nikolic67 <nesamskolovao2@gmail.com>
2023-11-22 09:27:45 +0100

    Finished WordGuesser

--- src/at/dragan/Projekte/WordGuesser.java
+++ src/at/dragan/Projekte/WordGuesser.java
@@ -22,5 +22,41 @@ public class WordGuesser {
         Scanner scanner = new Scanner(System.in);
 
 
+        while (true) {
+
+            System.out.println("Aktuelles Rätsel: " + new String(raetsel));
+
+
+            System.out.print("Welcher Buchstabe könnte im gesuchten Wort stecken? ");
+            char eingabe = scanner.next().charAt(0);
+
+
+            boolean buchstabeRichtig = false;
+            for (int i = 0; i < gesuchtesWort.length(); i++) {
+                if (Character.toLowerCase(gesuchtesWort.charAt(i)) == Character.toLowerCase(eingabe)) {
+                    raetsel[i] = gesuchtesWort.charAt(i);
+                    buchstabeRichtig = true;
+                }
+            }
+
+
+            if (new String(raetsel).equals(gesuchtesWort)) {
+                System.out.println("Herzlichen Glückwunsch! Du hast das Wort richtig geraten: " + gesuchtesWort);
+                break;
+            }
+
+
+            if (buchstabeRichtig) {
+                System.out.println("Richtig");
+            } else {
+                System.out.println("Leider falsch :( ");
+            }
+        }
+
+
+        scanner.close();
     }
 }
+
+
+
